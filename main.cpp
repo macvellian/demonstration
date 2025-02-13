94cb5247a38221b2d07d1bf04216ef55c816b5f0
Max <mamackay@uvm.edu>
2025-02-12 19:39:31 -0500

    I also changed main again

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Good evening, World!" << std::endl;
 
     return 0;
 }
